9905c65d281aa8c5b7afb200ba262860d7f2d1c8
emilka300 <47722177+emilka300@users.noreply.github.com>
2022-05-11 21:35:52 +0200

    demo from tutorial - just to understand how use it

---
+++ contacts.py
@@ -0,0 +1,15 @@
+
+# we just what to get all email addresses from database
+contacts = {
+    "number of students": 4,
+    "data":
+    [
+        {"name": "Sarah Holdrness", "email": "sarag@example.com"},
+        {"name": "Harry Potter", "email": "harry@example.com"},
+        {"name": "Hermione Granger", "email": "hermione@example.com"},
+        {"name": "Ron Wesley", "email": "ron@example.com"}
+    ]
+}
+
+for student in contacts["data"]:
+    print(student["email"])
