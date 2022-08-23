6fdbfe846d0657af2328a4fa1c042f14b87b0a6a
emilka300 <47722177+emilka300@users.noreply.github.com>
2022-08-23 13:57:42 +0200

    working with dictionaries

--- contacts.py
+++ contacts.py
@@ -1,15 +1,14 @@
-
 # we just what to get all email addresses from database
 contacts = {
     "number of students": 4,
     "data":
-    [
-        {"name": "Sarah Holdrness", "email": "sarag@example.com"},
-        {"name": "Harry Potter", "email": "harry@example.com"},
-        {"name": "Hermione Granger", "email": "hermione@example.com"},
-        {"name": "Ron Wesley", "email": "ron@example.com"}
-    ]
+        [
+            {"name": "Sarah Holdrness", "email": "sarag@example.com"},
+            {"name": "Harry Potter", "email": "harry@example.com"},
+            {"name": "Hermione Granger", "email": "hermione@example.com"},
+            {"name": "Ron Wesley", "email": "ron@example.com"}
+        ]
 }
-
+print(contacts["number of students"])
 for student in contacts["data"]:
     print(student["email"])
