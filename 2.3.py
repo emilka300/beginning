1889814317e000d43944427d5306ba85c654e160
Emilia Wach <47722177+emilka300@users.noreply.github.com>
2022-11-08 13:36:55 +0100

    Shortened version of element's sum; added descended sorting; correction in last 3 numbers [-3:]

--- 2.3.py
+++ 2.3.py
@@ -6,19 +6,28 @@ print(len(lista))
 print(f' Maximum value: {max(lista)} \n Minimum value: {min(lista)}')
 
 # b.	Wyświetl sumę elementów listy
-suma = 0
+suma = sum(lista)
 
-for i in lista:
-    suma = suma + i
+# longer version
+# suma = 0
+# for i in lista:
+#     suma = suma + i
 
 print(f' Sum = {suma}')
 
 # c.	Uporządkuj listę rosnąco i malejąco
-sorted = sorted(lista)
-print(sorted)
+# https://docs.python.org/3/howto/sorting.html#ascending-and-descending <- w razie gdybym potrzebowała ciekawiej
+sorted_ascending = sorted(lista)
+# both options are valid
+sorted_descending_v1 = sorted(lista, reverse=True)
+sorted_descending_v2 = sorted_ascending[::-1]
+print(f' List ascending sorted: {sorted_ascending}\nList descending sorted {sorted_descending_v1, sorted_descending_v2}')
 
 # d.	Wyświetl trzy najmniejsze i trzy największe liczby
-print(f' Three numbers from the beginning: {sorted[0:3]} \n Three numbers from the end {sorted[22: 25]}')
+print(f' Three numbers from the beginning (ascended list): {sorted_ascending[0:3]} \n Three numbers from the end\
+{sorted_ascending[-3:]}')
+print(f' Three numbers from the beginning (descended list): {sorted_descending_v1[0:3]} \n Three numbers from the end\
+{sorted_descending_v1[-3:]}')
 
 # e.	Usuń duplikaty
 
